660e8615fed58aec2bd45b68099f55896bb98b9f
Igor Marques <igormarjes@gmail.com>
2015-06-08 18:40:13 -0300

    Alteração usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+        belongs_to :sector_origin, class_name: "Person::Sector"
+        belongs_to :sector_current, class_name: "Person::Sector"
 end
