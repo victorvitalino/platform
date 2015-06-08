6089a95fea5354f6d861d037e0936e2dbe81b578
Victor Vitalino <vitalino.victor@gmail.com>
2015-06-08 17:00:06 -0300

    layout posts e pages

--- engines/cms/app/models/cms/page.rb
+++ engines/cms/app/models/cms/page.rb
@@ -1,5 +1,6 @@
 module Cms
   class Page < ActiveRecord::Base
     belongs_to :page_category
+    validates_presence_of :title, :content
   end
 end
--- engines/cms/app/models/cms/post.rb
+++ engines/cms/app/models/cms/post.rb
@@ -1,5 +1,6 @@
 module Cms
   class Post < ActiveRecord::Base
     belongs_to :post_category
+    validates_presence_of :title, :content
   end
 end
