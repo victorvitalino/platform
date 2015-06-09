d549e802a1424d895aaa51fec657d466bd9cae2a
Igor <igormarjes@gmail.com>
2015-06-09 10:31:16 -0300

    qulquer coisa

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
 
         belongs_to :sector_origin, class_name: "Person::Sector"
         belongs_to :sector_current, class_name: "Person::Sector"
+
+  validates_presence_of :name, :code, :email, :cpf, :rg, :rg_org, :blood_type
+  validates_presence_of :encrypted_password, :status, :avatar
+
+  validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]+\z/i
+
+  #mount_uploader :avatar, AvatarUploader
 end
