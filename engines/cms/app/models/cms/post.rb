0cf8222f1e882aa6e785bbd52c3abe809f5749f4
Victor Vitalino <vitalino.victor@gmail.com>
2015-06-09 14:52:07 -0300

    CarrierWave Model + path

--- engines/cms/app/models/cms/post.rb
+++ engines/cms/app/models/cms/post.rb
@@ -2,5 +2,7 @@ module Cms
   class Post < ActiveRecord::Base
     belongs_to :post_category
     validates_presence_of :title, :content, :post_category
+
+    mount_uploader :thumb, ThumbUploader
   end
 end
