822a243a061a5e29ef44def8c9ed5c4a71b126a4
Victor Vitalino <vitalino.victor@gmail.com>
2015-06-28 13:54:28 -0300

    fix

--- engines/cms/app/models/cms/post.rb
+++ engines/cms/app/models/cms/post.rb
@@ -1,7 +1,7 @@
 module Cms
   class Post < ActiveRecord::Base
     belongs_to :post_category
-    validates_presence_of :title, :content, :post_category
+    validates_presence_of :title, :content, :post_category, :date
 
     mount_uploader :thumb, ThumbUploader
     mount_uploader :thumbnail, ThumbnailUploader
