3e8da1c48f96724c925b49d840bb42157b1ebc0a
maxbruno <brunomax18@gmail.com>
2015-07-17 14:10:05 -0300

    helper helpdesk

--- engines/helpdesk/app/controllers/helpdesk/application_controller.rb
+++ engines/helpdesk/app/controllers/helpdesk/application_controller.rb
@@ -1,6 +1,7 @@
 module Helpdesk
   class ApplicationController < ActionController::Base
   	layout 'layouts/material'
+  	helper ::ApplicationHelper
   	include Pundit
   	rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
