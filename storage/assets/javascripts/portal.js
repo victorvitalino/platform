16715eb483e1482fbaf5dd3c1ef524d899784daa
Elton Silva <elton.chrls@gmail.com>
2015-08-24 12:45:08 -0300

    ok

--- storage/assets/javascripts/portal.js
+++ storage/assets/javascripts/portal.js
@@ -1,2 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+//= require portal/js/script
+//= require portal/js/bootstrap.min
+//= require portal/js/jquery.flexslider
+//= require portal/js/slider
+//= require portal/js/jquery.mask.min
+//= require portal/js/mask
+//= require portal/js/portal
