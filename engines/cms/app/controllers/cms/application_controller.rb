92f565b101eb9259af39896e5701f5d7d6a3da60
Victor Vitalino <vitalino.victor@gmail.com>
2015-08-31 15:57:12 -0300

    fix

--- engines/cms/app/controllers/cms/application_controller.rb
+++ engines/cms/app/controllers/cms/application_controller.rb
@@ -8,29 +8,13 @@ module Cms
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
     helper ::ApplicationHelper
-<<<<<<< HEAD
 
-  #  before_action :redirect_first_access
 
-=======
-  
-  
->>>>>>> 0f9fe68dabaf153cfb952e93fc2a8cf71db853f2
     private
 
     def user_not_authorized(exception)
       redirect_to('/404')
     end
-<<<<<<< HEAD
-
-    def redirect_first_access
-      if current_user.sign_in_count == 1 && !devise_controller?
-        flash[:info] = "Você precisa alterar sua senha para seguir com o acesso normalmente."
-        redirect_to main_app.edit_user_registration_path
-      end
-    end
   end
-=======
- end
->>>>>>> 0f9fe68dabaf153cfb952e93fc2a8cf71db853f2
+
 end
