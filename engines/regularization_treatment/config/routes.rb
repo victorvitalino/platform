fa35e940813da426f73fc7685b2efe3dac2827f0
Elton Silva <elton.chrls@gmail.com>
2015-09-17 11:03:52 -0300

    novos campos

--- engines/regularization_treatment/config/routes.rb
+++ engines/regularization_treatment/config/routes.rb
@@ -1,11 +1,16 @@
 RegularizationTreatment::Engine.routes.draw do
-  resources :consults
-  resources :requeriments
-  resources :addresses
-  resources :cadastres
-  resources :kins
-  resources :checklist_treatments
+
+  resources :consults, path: 'consulta'
+  
+  resources :requeriments, path: 'requerimento' do 
+    resources :addresses, path: 'endereco'
+    resources :cadastres, path: 'cadastro'
+    resources :kins,  path: 'dependentes'
+    resources :checklist_treatments, path: 'checklist'
+  end
+
   resources :cadastre_procedural_statuses
   resources :authenticates
+
 
 end
