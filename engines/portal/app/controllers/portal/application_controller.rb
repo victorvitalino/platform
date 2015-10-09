42913cbd8b8e155fbb8cb09c710db59589b03550
Elton Silva <elton.chrls@gmail.com>
2015-10-09 16:56:29 -0300

    alterações layout

--- engines/portal/app/controllers/portal/application_controller.rb
+++ engines/portal/app/controllers/portal/application_controller.rb
@@ -2,7 +2,7 @@ require_dependency 'application_helper'
 require_dependency 'portal/application_helper'
 module Portal
   class ApplicationController < ActionController::Base
-    layout 'portal/application'
+    layout 'portal'
     helper ::ApplicationHelper
   end
 end
