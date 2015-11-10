850ae15bce4275b01729b2b25635e40aa551de7e
Elton Silva <elton.chrls@gmail.com>
2015-11-10 16:16:35 -0200

    permissões

--- engines/helpdesk/app/controllers/helpdesk/application_controller.rb
+++ engines/helpdesk/app/controllers/helpdesk/application_controller.rb
@@ -1,7 +1,11 @@
+require_dependency 'application_helper'
+
 module Helpdesk
   class ApplicationController < ActionController::Base
     layout 'material'
-    
+  
+    helper 'application'
+        
     include Pundit
   end
 end
