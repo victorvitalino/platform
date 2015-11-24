c97ebb7447302fb3e9709673fa878f70f3af0776
Elton Silva <elton.chrls@gmail.com>
2015-11-24 15:27:51 -0200

    agendamento de regularização

--- engines/regularization/config/routes.rb
+++ engines/regularization/config/routes.rb
@@ -2,7 +2,9 @@ Regularization::Engine.routes.draw do
   root 'home#index'
 
   resources :agendas do 
-    resources :schedules
+    resources :schedules do 
+      delete 'cancel'
+    end
   end
 end
 
