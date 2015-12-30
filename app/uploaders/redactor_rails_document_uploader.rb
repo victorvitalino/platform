6faa39d6e02592ef0113ad4e29a423b657e97ecc
Elton Silva <elton.chrls@gmail.com>
2015-12-30 16:00:36 -0200

    ok

--- app/uploaders/redactor_rails_document_uploader.rb
+++ app/uploaders/redactor_rails_document_uploader.rb
@@ -2,7 +2,9 @@
 class RedactorRailsDocumentUploader < CarrierWave::Uploader::Base
   include RedactorRails::Backend::CarrierWave
 
-  storage :sftp
+  storage :file if Rails.env.development? || Rails.env.test?
+  storage :sftp if Rails.env.production?
+
 
   def store_dir
     "content/files"
