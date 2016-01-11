61e72b3f8a05cd0a5d12f218fa2715427d5df0db
Elton Silva <elton.chrls@gmail.com>
2016-01-11 16:43:42 -0200

    ok

--- engines/concourse_portal/app/mailers/concourse_portal/subscribe_mailer.rb
+++ engines/concourse_portal/app/mailers/concourse_portal/subscribe_mailer.rb
@@ -2,10 +2,10 @@ module ConcoursePortal
   class SubscribeMailer < ActionMailer::Base
     default from: 'concursos@codhab.df.gov.br'
 
-    def success(candidate, project)
+    def success(email, candidate, project)
       @project    = project
       @candidate  = candidate
-      mail(to: @candidate.email, subject: "CODHAB - #{project.title}- Inscrição Recebida")
+      mail(to: email, subject: "CODHAB - #{project.title}- Inscrição Recebida")
     end
   end
 end
