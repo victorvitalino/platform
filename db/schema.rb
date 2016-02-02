97a44d5eae0853225a9009feb24b8e2841630356
Igor Marques <igormarjes@gmail.com>
2016-02-02 16:51:44 -0200

    tretas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202121259) do
+ActiveRecord::Schema.define(version: 20160202184610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -680,6 +680,12 @@ ActiveRecord::Schema.define(version: 20160202121259) do
     t.datetime "updated_at",                 null: false
   end
 
+  create_table "candidate_regularization_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "candidate_signed_instruments", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -889,6 +895,7 @@ ActiveRecord::Schema.define(version: 20160202121259) do
     t.integer  "project_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.boolean  "responded"
   end
 
   add_index "concourse_consults", ["project_id"], name: "index_concourse_consults_on_project_id", using: :btree
--- engines/candidate/app/controllers/candidate/cadastre_addresses_controller.rb
+++ engines/candidate/app/controllers/candidate/cadastre_addresses_controller.rb
@@ -5,18 +5,18 @@ module Candidate
 
 
     def index
-      @address = RegularizationPortal::Address.new
-       if  params[:cpf].present?
+       @address = RegularizationPortal::Address.new
+       unless  params[:cpf].nil? && params[:cpf].empty?
         @candidate = Candidate::CadastreAddress.joins(:unit).joins(:cadastre).where('candidate_cadastres.cpf = ?', params[:cpf]).last rescue nil
+       end
+       unless params[:id].nil?
+        @candidate = Candidate::CadastreAddress.joins(:unit).where('address_units.id = ?', params[:id]).last rescue nil
        end
     end
 
 
-
-
     def edit
-      @address = RegularizationPortal::Address.new
-      @candidate = Candidate::CadastreAddress.joins(:unit).joins(:cadastre).where('candidate_cadastres.cpf = ?', params[:cpf]).last rescue nil
+      @candidate = Candidate::CadastreAddress.joins(:unit).where('address_units.id = ?', params[:id]).last rescue nil
     end
 
 
