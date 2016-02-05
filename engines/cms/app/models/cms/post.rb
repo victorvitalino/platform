0d47a104488e93dee4966322837f94af80c87b2b
Elton Silva <elton.chrls@gmail.com>
2016-02-05 10:19:22 -0200

    remove boosty

--- engines/cms/app/models/cms/post.rb
+++ engines/cms/app/models/cms/post.rb
@@ -1,8 +1,6 @@
 module Cms
   class Post < ActiveRecord::Base
 
-    include Bootsy::Container
-
     
     belongs_to :post_category
     validates_presence_of :title, :content, :post_category, :date
