583773379c00a81da27eff5a98c722c5d9a8f142
Elton Silva <elton.chrls@gmail.com>
2016-02-16 15:07:47 -0200

    correção de lista

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:assets_precompile'
+    #invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
     invoke :'log:unicorn:clean'
     invoke :'log:app:clean'
