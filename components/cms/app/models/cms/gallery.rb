39addb61ee8f68ca90f3a0eff994aa2e36e90d9b
Elton Silva <elton.chrls@gmail.com>
2016-02-23 11:47:03 -0300

    fix gallery dependency

--- components/cms/app/models/cms/gallery.rb
+++ components/cms/app/models/cms/gallery.rb
@@ -4,7 +4,7 @@ module Cms
   validates_presence_of :title
 
 
-  validates :photo, :thumbnail
+ # validates :photo, :thumbnail
   validates :photo, :thumbnail, file_size: { less_than_or_equal_to: 10.megabytes.to_i }
   validates :photo, :thumbnail, file_content_type: { allow: ['image/jpeg', 'image/png'],
                                               message: 'Somente arquivos .jpg ou .png' }
