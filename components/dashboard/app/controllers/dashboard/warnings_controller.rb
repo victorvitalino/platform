e0aa071be4ce255aab4a9454d64dbdc3ba56b3e8
Victor Vitalino <vitalino.victor@gmail.com>
2016-03-07 17:01:52 -0300

    dashboard controller

--- components/dashboard/app/controllers/dashboard/warnings_controller.rb
+++ components/dashboard/app/controllers/dashboard/warnings_controller.rb
@@ -1,21 +1,48 @@
+require_dependency "Dashboard/application_controller"
+
 module Dashboard
   class WarningController < ApplicationController
     def index
+      @warnings = Warning.all
+    end
+
+    def show
+    end
+
+    def new
+      @warning = Warning.new
     end
 
     def create
+      @warning = Warning.new(warning_params)
+      if @warning.save
+        redirect_to action: 'index'
+      else
+        render :new
+      end
     end
 
     def update
+      if @warning.update(warning_params)
+        redirect_to action: 'index'
+      else
+        render :edit
+      end
     end
 
     def destroy
+      @warning.destroy
+      redirect_to warnings_url, notice: 'Warning was successfully destroyed.'
     end
 
-    
+
     private
+    def set_warning
+      @warning = Dashboard::Warning.find(params[:id])
+    end
 
     def warning_params
+      params.require(:warning).permit(:title, :content, :resume, :date, :publish)
     end
   end
 end
