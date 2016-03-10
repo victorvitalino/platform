02a530e7804b94161d34332339e5475aa7c7548c
Elton Silva <elton.chrls@gmail.com>
2016-03-10 14:29:43 -0300

    ok

--- components/concourse/app/models/concourse/winner.rb
+++ components/concourse/app/models/concourse/winner.rb
@@ -7,16 +7,29 @@ module Concourse
 
     enum winner_type: ['menção_honorosa', 'terceiro_lugar', 'segundo_lugar', 'primeiro_lugar']
     
-    validates :participation, presence: true
-    validates_uniqueness_of :participation, scope: :project 
     validate :project_exists?
 
+    def self.winner_custom_types(project_id)
+      self.winner_types.map do |k, v|
+        [k,v]
+      end
+    end
+
     private
+
 
     def project_exists?
       @participation = Concourse::CandidateParticipation.find(self.participation_id) rescue nil
 
-      errors.add(:participation_id, "Projeto não existe") if @participation.nil?
+      if @participation.nil?
+        errors.add(:participation_id, "Projeto não existe") 
+      else
+        @winner = Concourse::Winner.find_by_participation_id(@participation.id) rescue nil
+        errors.add(:participation_id, "Projeto já classificado") if @winner.present?  
+      end
+
+      errors.add(:winner_type, 'já noemado') if self.winner_type == nil
     end
+
   end
 end
