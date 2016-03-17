2f633df3f714fcc09c4e8cb393b02f5ece7ff6fa
Victor Vitalino <vitalino.victor@gmail.com>
2016-03-17 15:22:16 -0300

    nov

--- components/portal/app/controllers/portal/home_controller.rb
+++ components/portal/app/controllers/portal/home_controller.rb
@@ -3,6 +3,9 @@ module Portal
   class HomeController < ApplicationController
     def index
     end
-
+    def self.search(query)
+      # where(:title, query) -> This would return an exact match of the query
+      @post = Cms::Post.where("title like ?", "%#{query}%")
+    end
   end
 end
