c0047fcc12e3dc49bab79cb978fd0fd5798aa627
Igor Marques <igormarjes@gmail.com>
2016-05-02 16:26:00 -0300

    Protocolo

--- components/protocol/app/controllers/protocol/reports_controller.rb
+++ components/protocol/app/controllers/protocol/reports_controller.rb
@@ -3,27 +3,40 @@ require_dependency 'protocol/application_controller'
 module Protocol
     class ReportsController < ApplicationController
 
+       has_scope :date_start
+       has_scope :date_end
+       has_scope :subject
+       has_scope :doc_type
+       has_scope :sector
+
         def new
           authorize :report,  :create?
           #@report = Report.new
 
               if params[:date_start].present? && params[:date_end].present?
-                @date_start = Date.parse(params[:date_start])
-                @date_end  = Date.parse(params[:date_end])
                 @assessments = Assessment.joins(:subject,:document_type).
-                select('document_number, protocol_document_types.name as doc_type_name, external_agency, protocol_subjects.name as subject_name, requesting_unit').
-                where("protocol_assessments.created_at::date >= ?  AND protocol_assessments.created_at::date <= ? ", @date_start ,  @date_end )
-              else
-                @assessments = Assessment.joins(:subject,:document_type).
-                select('document_number, protocol_document_types.name as doc_type_name, external_agency, protocol_subjects.name as subject_name, requesting_unit').
-                where("protocol_assessments.created_at::date >= ?", Date.today)
+                select('document_number, protocol_document_types.name as doc_type_name, external_agency, protocol_subjects.name as subject_name, requesting_unit')
+                @assessments = apply_scopes(@assessments)
               end
 
               respond_to do |format|
                 format.html
                 format.csv { send_data @assessments.to_csv }
-
               end
+        end
+
+        def general_report
+
+          if params[:date_start].present? || params[:date_end].present? || params[:doc_type].present? || params[:subject].present? || params[:sector].present?
+            @assessments = Assessment.joins(:subject,:document_type).
+            select('document_number,recipient, protocol_assessments.created_at, protocol_document_types.name as doc_type_name,protocol_subjects.name as subject_name, requesting_unit')
+            @assessments = apply_scopes(@assessments)
+          end
+
+          respond_to do |format|
+            format.html
+            format.csv { send_data @assessments.to_csv }
+          end
 
         end
 
@@ -31,6 +44,6 @@ module Protocol
 
         def set_params
             params.require(:report).permit(:date_start, :date_end)
-         end
+        end
     end
 end
