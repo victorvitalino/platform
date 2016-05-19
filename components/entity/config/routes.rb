01fc4bf507eef3685b44b326c458e3cf25797352
Elton Silva <elton.chrls@gmail.com>
2016-05-19 10:13:16 -0300

    update members and documents

--- components/entity/config/routes.rb
+++ components/entity/config/routes.rb
@@ -1,5 +1,7 @@
 Entity::Engine.routes.draw do
-  resources :cadastres, path: 'cadastros'
+  resources :cadastres, path: 'cadastros' do 
+    resources :members
+  end
   resources :document_categories, path: 'categorias_documento'
   resources :member_jobs, path: 'diretoria_cargos'
 end
