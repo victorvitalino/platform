6c4996c6f4c2dccf1ecccfd9229f99f75f18d26d
Igor Marques <igormarjes@gmail.com>
2016-06-09 12:40:10 -0300

    logs do sistema

--- components/candidate/app/models/candidate/cadastre_activity.rb
+++ components/candidate/app/models/candidate/cadastre_activity.rb
@@ -6,5 +6,30 @@ module Candidate
 
     enum type_activity:   ['simples', 'judicial','crítico', 'corretiva', 'sistema']
     enum type_ocurrency:  ['impeditivo', 'informativo', 'corretivo']
+
+    def self.create_simple_log!(options = {})
+      cadastre_id        = options[:cadastre_id] ||= nil
+      staff_id           = options[:staff_id] ||= nil
+      activity_status_id = options[:activity_status_id] ||= nil
+      type_activity      = options[:type_activity] ||= nil
+      status             = options[:status] ||= nil
+      type_ocurrency     = options[:type_ocurrency] ||= nil
+      observation        = options[:observation] ||= nil
+      cadastre_mirror_id = options[:cadastre_mirror_id] ||= nil
+
+
+      object = self.new
+      object.cadastre_id = cadastre_id
+      object.staff_id = staff_id
+      object.activity_status_id = activity_status_id
+      object.type_activity = type_activity
+      object.status = status
+      object.type_ocurrency = type_ocurrency
+      object.observation = observation
+      object.cadastre_mirror_id = cadastre_mirror_id
+
+      object.save
+    end
+
   end
 end
