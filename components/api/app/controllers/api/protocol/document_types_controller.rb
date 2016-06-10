b5b7b55cfcc76febf490656ecd6daa413bc52df3
Igor Marques <igormarjes@gmail.com>
2016-06-10 15:41:27 -0300

    detalhes protocolo

--- components/api/app/controllers/api/protocol/document_types_controller.rb
+++ components/api/app/controllers/api/protocol/document_types_controller.rb
@@ -3,12 +3,24 @@ require_dependency 'api/application_controller'
 module Api
   module Protocol
     class DocumentTypesController < ApplicationController
-    
+
       def index
         @documents = ::Protocol::DocumentType.all.order(:name)
         render json: @documents
       end
-      
+
+      def subject_direg
+        return false unless params[:id].present?
+        if params[:id].to_i == 66
+          @doc_type = ::Protocol::Subject.where(description: "DIREG")
+        else
+          @doc_type = ::Protocol::Subject.all
+        end
+        render json: @doc_type
+
+      end
+
+
     end
   end
 end
