c6f94910c582ac37ab69dbc7b46c388efbfc3f39
Elton Silva <elton.chrls@gmail.com>
2016-06-24 17:37:17 -0300

    ok

--- components/portal/app/controllers/portal/habita_controller.rb
+++ components/portal/app/controllers/portal/habita_controller.rb
@@ -1,7 +1,12 @@
 require_dependency 'portal/application_controller'
 module Portal
   class HabitaController < ApplicationController
+    
     def index
+      
+      unless Date.today > Date.parse('24/06/2016') || params[:quero_ver].present?
+        render json: "Não disponível"
+      end
     end
 
   end
