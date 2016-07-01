ec3d0d7ef201d21951b52cde07c06ed597172023
Victor Vitalino <vitalino.victor@gmail.com>
2016-07-01 14:47:39 -0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628181319) do
+ActiveRecord::Schema.define(version: 20160629181259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1198,6 +1198,7 @@ ActiveRecord::Schema.define(version: 20160628181319) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "flickr"
+    t.string   "color"
   end
 
   add_index "cms_posts", ["post_category_id"], name: "index_cms_posts_on_post_category_id", using: :btree
