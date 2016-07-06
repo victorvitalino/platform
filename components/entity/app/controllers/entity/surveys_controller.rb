d1352bc71e42e74e0c89abcd9526010b1b6c2f48
Victor Vitalino <vitalino.victor@gmail.com>
2016-07-06 17:21:43 -0300

    fix cnpj filter

--- components/entity/app/controllers/entity/surveys_controller.rb
+++ components/entity/app/controllers/entity/surveys_controller.rb
@@ -4,8 +4,12 @@ module Entity
     layout 'patternfly/mobile-less-application'
 
   def index
-    @surveys = Entity::Cadastre.all
+    if params[:by_cnpj].present?
+      @surveys = Entity::Cadastre.where(cnpj:params[:by_cnpj].to_s.unformat_cnpj)
+    else
+      @surveys = Entity::Cadastre.all
+    end
   end
-  
-end
+
+  end
 end
