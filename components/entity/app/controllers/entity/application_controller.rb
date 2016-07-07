6e9ec1526c550acc2190cd5b36c55d532b120c91
Victor Vitalino <vitalino.victor@gmail.com>
2016-07-07 09:55:29 -0300

    iframe

--- components/entity/app/controllers/entity/application_controller.rb
+++ components/entity/app/controllers/entity/application_controller.rb
@@ -8,11 +8,19 @@ module Entity
     helper  Authenticate::StaffHelper
     helper  ::NavigationHelper
 
-    before_action :authenticate_staff?
+    # before_action :authenticate_staff?
 
     include Pundit
 
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+
+    before_action :allow_iframe
+
+    private
+    def allow_iframe
+      response.headers.delete('X-Frame-Options')
+    end
   end
+
 end
