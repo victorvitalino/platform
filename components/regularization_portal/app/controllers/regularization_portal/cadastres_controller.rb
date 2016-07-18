bf92b7ad1ec25da37f40b9dd0855d4c972a4db50
Elton Silva <elton.chrls@gmail.com>
2016-07-18 11:41:04 -0300

    add cors

--- components/regularization_portal/app/controllers/regularization_portal/cadastres_controller.rb
+++ components/regularization_portal/app/controllers/regularization_portal/cadastres_controller.rb
@@ -2,6 +2,7 @@ require_dependency 'regularization_portal/application_controller'
 
 module RegularizationPortal
   class CadastresController < ApplicationController 
+    before_action :set_cors_headers
     
     def show
       @candidate = ::Candidate::Cadastre.regularization.find_by_cpf(params[:id])
@@ -35,6 +36,22 @@ module RegularizationPortal
       params.require(:find).permit(:cpf)
     end
 
+    def add_cors_headers
+      origin = request.headers["Origin"]
+      unless (not origin.nil?) and (origin == "http://localhost" or origin.starts_with? "http://localhost:")
+        origin = "http://www.codhab.df.gov.br/"
+      end
+      headers['Access-Control-Allow-Origin'] = origin
+      headers['Access-Control-Allow-Methods'] = 'POST, GET'
+      allow_headers = request.headers["Access-Control-Request-Headers"]
+      if allow_headers.nil?
+        #shouldn't happen, but better be safe
+        allow_headers = 'Origin, Authorization, Accept, Content-Type'
+      end
+      headers['Access-Control-Allow-Headers'] = allow_headers
+      headers['Access-Control-Allow-Credentials'] = 'true'
+      headers['Access-Control-Max-Age'] = '1728000'
+    end
     
   end
 end
