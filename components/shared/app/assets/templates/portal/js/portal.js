d9a129d2d8a2a4332de17a9fb2641eaf18f49163
Victor Vitalino <vitalino.victor@gmail.com>
2016-07-20 09:02:50 -0300

    fix

--- components/shared/app/assets/templates/portal/js/portal.js
+++ components/shared/app/assets/templates/portal/js/portal.js
@@ -1,53 +1,40 @@
 $(document).ready(function() {
-  // var mes = 'Jan';
-  // var ano = 'segundo_ano';
-  // var botao = '';
-  // var botao_ano = '';
-  // var current = mes;
-  // var current_ano = ano;
-  // $('.'+ mes).css('display','block');
-  //
-  // $('.tab-mes span').click(function(){
-  //   botao = $(this).attr('data-mes');
-  //   if(current!=botao){
-  //     $('.s-info').css('display','none');
-  //     $('.'+ botao).css('display','block');
-  //     current = botao;
-  //   }
-  // });
-  //
-  // $('.'+ ano).css('display','block');
-  //
-  // $('.tab-ano li a span').click(function(){
-  //   botao_ano = $(this).attr('data-ano');
-  //   if(current_ano!=botao_ano){
-  //     $('.flip_ano').css('display','none');
-  //     $('.'+ botao_ano).css('display','block');
-  //     current_ano = botao_ano ;
-  //     if(current_ano == 'primeiro_ano'){
-  //       botao = 'Mai';
-  //       $('.s-info').css('display','none');
-  //       $('.'+ botao).css('display','block');
-  //     }else{
-  //       botao = 'Jan';
-  //       $('.s-info').css('display','none');
-  //       $('.'+ botao).css('display','block');
-  //     }
-  //   }
-  // });
+  var mes = 'Jan';
+  var ano = 'segundo_ano';
+  var botao = '';
+  var botao_ano = '';
+  var current = mes;
+  var current_ano = ano;
+  $('.'+ mes).css('display','block');
 
-//   $('.number_metas').each(function(){
-//     $(this).prop('Counter',0).animate({
-//       Counter: $(this).text()
-//     },{
-//       duration: 1000,
-//       easing: 'swing',
-//       step: function (now){
-//         $(this).text(Math.ceil(now));
-//       }
-//     });
-//   });
-// });
+  $('.tab-mes span').click(function(){
+    botao = $(this).attr('data-mes');
+    if(current!=botao){
+      $('.s-info').css('display','none');
+      $('.'+ botao).css('display','block');
+      current = botao;
+    }
+  });
+
+  $('.'+ ano).css('display','block');
+
+  $('.tab-ano li a span').click(function(){
+    botao_ano = $(this).attr('data-ano');
+    if(current_ano!=botao_ano){
+      $('.flip_ano').css('display','none');
+      $('.'+ botao_ano).css('display','block');
+      current_ano = botao_ano ;
+      if(current_ano == 'primeiro_ano'){
+        botao = 'Mai';
+        $('.s-info').css('display','none');
+        $('.'+ botao).css('display','block');
+      }else{
+        botao = 'Jan';
+        $('.s-info').css('display','none');
+        $('.'+ botao).css('display','block');
+      }
+    }
+  });
 
 $(document).ready(function() {
   $('form').on('click', '.remove_fields', function(event) {
