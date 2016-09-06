c8c12263c26b61864667b75880458e5cd60a06cd
igormarjes <igormarjes@gmail.com>
2016-09-06 19:09:45 -0300

    dashboard

--- components/dashboard/app/controllers/dashboard/home_controller.rb
+++ components/dashboard/app/controllers/dashboard/home_controller.rb
@@ -3,8 +3,11 @@ require_dependency 'dashboard/application_controller'
 module Dashboard
 	class HomeController < ApplicationController
 
-  	def index			
-	  end
+  	def index
+			  @tickets = Helpdesk::Ticket.where(requester_id: current_user.id).order('created_at DESC')
+				@birthday_person = Person::Staff.where("status = true and date_part('month', born) = ?", Date.today.month).order("date_part('day', born) asc")
+				@branch_line = Person::BranchLine.order(:telephone)
+		end
 
 	end
 end
--- components/person/app/models/person/branch_line.rb
+++ components/person/app/models/person/branch_line.rb
@@ -1,10 +1,10 @@
 module Person
   class BranchLine < ActiveRecord::Base
     validates_presence_of :telephone, :description
-    validates :telephone, :description, uniqueness: true
+    validates :description, uniqueness: true
 
     audited
-    
+
     belongs_to :sector
     has_many :staffs
 
