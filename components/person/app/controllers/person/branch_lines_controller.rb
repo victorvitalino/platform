d35c39391580b6288634548f50af546129623f12
igormarjes <igormarjes@gmail.com>
2016-09-09 09:45:05 -0300

    ramais

--- components/person/app/controllers/person/branch_lines_controller.rb
+++ components/person/app/controllers/person/branch_lines_controller.rb
@@ -3,10 +3,12 @@ require_dependency "person/application_controller"
 module Person
 	class BranchLinesController < ApplicationController
 
+		before_action :set_sector
+		before_action :set_branch_lines, only: [:index, :new,:create, :destroy, :update]
 		before_action :set_branch_line, only: [:show,:edit, :destroy, :update]
 
 		def index
-			@branch_lines = BranchLine.all
+			authorize @branch_lines
 		end
 
 		def show
@@ -14,12 +16,12 @@ module Person
 		end
 
 		def new
-			@branch_line = BranchLine.new
+			@branch_line = @sector.branch_line.new
 			authorize @branch_line
 		end
 
 		def create
-			@branch_line = BranchLine.new(branch_line_params)
+			@branch_line = @sector.branch_line.new(branch_line_params)
 			authorize @branch_line
 			@branch_line.save
 
@@ -38,9 +40,16 @@ module Person
 			params.require(:branch_line).permit(:description, :telephone,:status, :sector_id)
 		end
 
+		def set_branch_lines
+			 @branch_lines = BranchLine.where(:sector_id => params[:sector_id])
+		end
+
+		def set_sector
+			@sector = Sector.find(params[:sector_id])
+		end
+
 		def set_branch_line
 			@branch_line = BranchLine.find(params[:id])
 		end
-
 	end
 end
